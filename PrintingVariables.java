736b3617237c1c8fc2049890bfe30ee915901278
suraee-1 <suraee30@gmail.com>
2025-04-22 12:21:08 +0530

    new file added

---
+++ PrintingVariables.java
@@ -0,0 +1,13 @@
+import java.rmi.server.SocketSecurityException;
+
+public class PrintingVariables {
+    public static void main(String []args){
+       
+        String name = "John";
+        int age = 18;
+        System.out.println("My name is "+name);
+        System.out.println("I am "+age+" years old.");
+        
+
+    }
+}
